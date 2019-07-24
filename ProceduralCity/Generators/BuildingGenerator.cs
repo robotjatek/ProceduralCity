242a9784d2a9225c485c53bb220d61954d02bade
Attila Szucs <szucsa@sonrisa.hu>
2019-07-24 10:25:01 +0200

    Generate multiple building types

--- ProceduralCity/Generators/BuildingGenerator.cs
+++ ProceduralCity/Generators/BuildingGenerator.cs
@@ -11,6 +11,13 @@ namespace ProceduralCity.Generators
 {
     class BuildingGenerator : IBuildingGenerator
     {
+        enum BuildingType
+        {
+            Simple,
+            Tower,
+            //  Blocky
+        }
+
         private readonly Shader _buildingShader;
         private readonly Vector2 _areaBorder;
         private readonly Random _random = new Random();
@@ -37,14 +44,32 @@ namespace ProceduralCity.Generators
                 var position = new Vector3(site.StartPosition.X + _areaBorder.X, 0, site.StartPosition.Y + _areaBorder.Y);
                 var area = site.EndPosition - site.StartPosition - _areaBorder;
                 var texture = _buildingTextures[_random.Next(_buildingTextures.Length)];
-
-                var building = new TowerBuilding(position, area, texture, _buildingShader, _random.Next(_config.MinBuildingHeight, _config.MaxBuildingHeight));
+                var building = CreateRandomBuilding(position, area, texture);
                 buildings.Add(building);
             }
 
             _logger.Information($"Number of buildings: {buildings.Count}");
 
             return buildings;
+        }
+
+        private IBuilding CreateRandomBuilding(Vector3 position, Vector2 area, Texture texture)
+        {
+            var type = (BuildingType)_random.Next(Enum.GetValues(typeof(BuildingType)).Length);
+            var height = _random.Next(_config.MinBuildingHeight, _config.MaxBuildingHeight);
+
+            switch (type)
+            {
+                case BuildingType.Simple:
+                    return new Building(position, area, texture, _buildingShader, height);
+                case BuildingType.Tower:
+                    return new TowerBuilding(position, area, texture, _buildingShader, height);
+                /*  case BuildingType.Blocky:
+                      return new BlockyBuilding(position, area, texture, _buildingShader, height); */
+                default:
+                    throw new NotImplementedException();
+            }
+
         }
 
         private bool disposedValue = false;
