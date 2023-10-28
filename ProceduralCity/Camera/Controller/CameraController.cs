dedb374a07b60ad61cd64f49f25ea0694b4d0089
Attila Szücs <ati.szucs@hotmail.com>
2023-10-28 21:13:21 +0200

    Restrict camera teleport range

--- ProceduralCity/Camera/Controller/CameraController.cs
+++ ProceduralCity/Camera/Controller/CameraController.cs
@@ -179,8 +179,8 @@ namespace ProceduralCity.Camera.Controller
             var rnd = new Random();
             // x, height, y;
             var height = rnd.Next(_configuration.MaxBuildingHeight + 10, _configuration.MaxBuildingHeight + 200);
-            var nx = (float)rnd.NextDouble() * _configuration.WorldSize;
-            var ny = (float)rnd.NextDouble() * _configuration.WorldSize;
+            var nx = (float)Math.Clamp(rnd.NextDouble(), 0.1f, 0.9f) * _configuration.WorldSize;
+            var ny = (float)Math.Clamp(rnd.NextDouble(), 0.1f, 0.9f) * _configuration.WorldSize;
 
             var pos = new Vector3(nx, height, ny);
             _camera.SetPosition(pos);
